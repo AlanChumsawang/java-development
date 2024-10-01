f293a19df86fc1ab17295d77667819908fbbde82
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-01 15:08:42 -0400

    Added formatting to annuity calculator answers. Applied logic allowing users to continue calculating until typing N or n.

--- workbook-1/finacialcalculator/src/main/java/com/pluralsight/finacialcalculator/AnnuityCalculator.java
+++ workbook-1/finacialcalculator/src/main/java/com/pluralsight/finacialcalculator/AnnuityCalculator.java
@@ -31,49 +31,54 @@ public class AnnuityCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter 1 to calculate the present value\nEnter 2 to calculate the monthly payment\nEnter 3 to calculate the total number of payments");
-        int userInput = scanner.nextInt();
+        boolean isDone = false;
+        while (!isDone) {
+            System.out.println("Enter 1 to calculate the present value\nEnter 2 to calculate the monthly payment\nEnter 3 to calculate the total number of payments");
+            int userInput = scanner.nextInt();
 
-        String getPresentValue = "Enter Present Value: ";
-        String getMonthlyPayment = "Enter Monthly Payment: ";
-        String getTotalNumberOfPayments = "Enter Total Number of Payments: ";
-        String getInterestRate = "Enter Annual Interest Rate: ";
+            String getPresentValue = "Enter Present Value: ";
+            String getMonthlyPayment = "Enter Monthly Payment: ";
+            String getTotalNumberOfPayments = "Enter Total Number of Payments: ";
+            String getInterestRate = "Enter Annual Interest Rate: ";
 
-        System.out.println(getInterestRate);
-        double interestRate = scanner.nextDouble() / 100;
-        AnnuityCalculator calculator = new AnnuityCalculator(interestRate);
+            System.out.println(getInterestRate);
+            double interestRate = scanner.nextDouble() / 100;
+            AnnuityCalculator calculator = new AnnuityCalculator(interestRate);
 
+            if (userInput == 1) {
+                System.out.println(getMonthlyPayment);
+                calculator.monthlyPayment = scanner.nextDouble();
+                System.out.println(getTotalNumberOfPayments);
+                calculator.totalNumberOfPayments = scanner.nextDouble();
+                calculator.findPresentValue();
+                System.out.printf("Present Value: %.2f\n", calculator.presentValue);
 
+            } else if (userInput == 2) {
+                System.out.println(getPresentValue);
+                calculator.presentValue = scanner.nextDouble();
+                System.out.println(getTotalNumberOfPayments);
+                calculator.totalNumberOfPayments = scanner.nextDouble();
+                calculator.findMonthlyPayment();
+                System.out.printf("Monthly Payment: %.2f\n", calculator.monthlyPayment);
 
-        if (userInput == 1) {
-            System.out.println(getMonthlyPayment);
-            calculator.monthlyPayment = scanner.nextDouble();
-            System.out.println(getTotalNumberOfPayments);
-            calculator.totalNumberOfPayments = scanner.nextDouble();
-            calculator.findPresentValue();
-            System.out.println("Present Value: " + calculator.presentValue);
+            } else if (userInput == 3) {
+                System.out.println(getPresentValue);
+                calculator.presentValue = scanner.nextDouble();
+                System.out.println(getMonthlyPayment);
+                calculator.monthlyPayment = scanner.nextDouble();
+                calculator.findTotalNumberOfPayments();
+                System.out.println("Total Number of Payments: " + calculator.totalNumberOfPayments);
 
+            } else {
+                System.out.println("Invalid input");
+            }
 
-        } else if (userInput == 2) {
-            System.out.println(getPresentValue);
-            calculator.presentValue = scanner.nextDouble();
-            System.out.println(getTotalNumberOfPayments);
-            calculator.totalNumberOfPayments = scanner.nextDouble();
-            calculator.findMonthlyPayment();
-            System.out.println("Monthly Payment: " + calculator.monthlyPayment);
-
-        } else if (userInput == 3) {
-            System.out.println(getPresentValue);
-            calculator.presentValue = scanner.nextDouble();
-            System.out.println(getMonthlyPayment);
-            calculator.monthlyPayment = scanner.nextDouble();
-            calculator.findTotalNumberOfPayments();
-            System.out.println("Total Number of Payments: " + calculator.totalNumberOfPayments);
-
-        } else {
-            System.out.println("Invalid input");
+            System.out.println("More Calculations? Y/N");
+            char answer = scanner.next().charAt(0);
+            if (answer == 'N' || answer == 'n') {
+                isDone = true;
+            }
         }
-
         scanner.close();
     }
 }
